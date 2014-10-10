0450abe0c95b6512dcb7f2820e4db9baf06ec209
三水清 <ksky521@gmail.com>
2014-10-10 21:00:54 +0800

    修改

--- tests/proxy.js
+++ tests/proxy.js
@@ -1,1 +1,31 @@
-//proxy.js
+var spider = require('../lib/spider');
+
+var now = Date.now();
+spider({
+    proxy: 'http://42.62.61.245:80',
+    uri: 'http://guangdiu.com',
+    timeout: 5e3
+}, function(data, error, req) {
+    if(!error && data){
+        var t = Date.now();
+        console.log(t - now);
+    }
+}, {
+    item: {
+        selector: '.gooditem',
+        handler: {
+            title: {
+                selector: 'h2',
+                handler: 'text'
+            },
+            prices: {
+                selector: 'h2 .emphricepart',
+                handler: 'text'
+            },
+            image: {
+                selector: '.showpic img',
+                handler: 'attr:src'
+            }
+        }
+    }
+});

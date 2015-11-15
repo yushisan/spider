b8bb4dda268effe963e23a441e84b81d5b0d4211
三水清 <ksky521@gmail.com>
2015-11-15 11:01:30 +0800

    添加gzip

---
+++ works/getproxy-cz88.js
@@ -0,0 +1,52 @@
+var spider = require('../lib/spider');
+var Queue = require('queue');
+var db = require('./_db');
+
+var testProxy = require('./test_proxy_speed');
+var uniqueObj = {};
+spider.get('http://www.cz88.net/proxy/index.aspx', function(data) {
+    var content = data.content;
+
+    var arr = [];
+    for (var i = 5, len = content.length; i < len; i++) {
+        var v = content[i];
+        if (/\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}/.test(v)) {
+            arr.push(v + ':' + content[i + 1]);
+            i+=5;
+        }
+    }
+
+    //获取代理list
+    // console.log(arr);
+    //并发一次的队列
+    var queue = new Queue({
+        concurrency: 1
+    });
+    arr.forEach(function(v) {
+        if (uniqueObj[v]) {
+            //检查重复的代理，减少系统消耗
+            return;
+        }
+        uniqueObj[v] = 1;
+        testProxy(v, 'http://guangdiu.com/m').then(function(data) {
+            //使用闭包来完成queue的封装
+            queue.push(function(cb) {
+                db.query('insert into proxy (proxy, speed) values (?,?)', [data.proxy, data.speed]).then(function() {
+                    console.log(data.proxy + ' → success');
+                    cb();
+                }, function(err) {
+                    console.log(err);
+                    cb();
+                });
+            });
+            queue.start();
+        }, function(err) {
+            // console.log(err);
+        });
+    });
+}, {
+    content: {
+        selector: '.Main table tr td',
+        handler: 'text'
+    }
+});

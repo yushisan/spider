d74f63271724b308b481dce02052be2fd590dbb5
wangyongqing01 <wangyongqing01@baidu.com>
2015-11-24 15:40:43 +0800

    cookie

--- tests/cookie.js
+++ tests/cookie.js
@@ -1,9 +1,9 @@
 var spider = require('../lib/spider');
 
-var cookie = 'BDUSS=EpHfll5amN2RGQ4UEl6RmxVWmtUNFI4akRzNkFVSlFyU1lNWXNMczY2T0tIWHBXQVFBQUFBJCQAAAAAAAAAAAEAAABXt4cAa3NreTUyMQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIqQUlaKkFJWN;';
+var cookie = 'BDUSS=121212;';
 
 spider.get({
-    url: 'http://po.m.baidu.com/api/user/get.json?format=json&appkey=searchbox',
+    url: '',
     cookie: cookie
 }, function(err, data) {
     console.log(data);
